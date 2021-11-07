b38355a57f0b87d5135c0a23bffc89703462c846
Felix Lugo <felixdlugo@gmail.com>
2021-11-07 10:30:15 -0400

    Added card and card-list component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-
+import { CardList } from './components/card-list/card-list.component';
 class App extends Component {
 constructor(){
   super();
   this.state={
-    string:'Hello Felix'
+    monsters: []
   };
 };
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(reponse=>reponse.json())
+  .then(users =>this.setState({monsters:users}))
+}
+
+
 render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        {this.state.string}
-        </p>
-        <button onClick={()=>this.setState({string:'Hello Yacira'})}>Click me!</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <CardList monsters={this.state.monsters}/>
     </div>
-  );
+  
+    );
 }
 
 }
