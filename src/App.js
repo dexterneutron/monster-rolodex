f241b5afbb866d78bbe20ad5a27e92a09f4578fc
Felix Lugo <felixdlugo@gmail.com>
2021-11-07 17:57:43 -0400

    corrected spelling

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ render() {
     )
   return (
     <div className="App">
-    <h1>Master Rolodex</h1>
+    <h1>Monster Rolodex</h1>
     <SearchBox
     placeholder="Search Monsters"
     handleChange={this.handleChange}
